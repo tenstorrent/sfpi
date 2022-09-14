8154f45406d5c149d4680e889191ab7b991f9500
Paul Keller <pkeller@tenstorrent.com>
2022-09-14 20:11:47 +0000

    Improve NOP scheduling for both GS/WH
    On GS: now handles NOPs in the scheduler rather than with a peephole
    
    Both: more intelligent with BBs (adds a nop if any child BB has a
    dependency).

---
+++ tests/grayskull/sfpi/schedule.cc
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "test.h"
+
+using namespace sfpi;
+
+void basic_nop(int imm)
+{
+    {
+        // imm store, imm load
+        vFloat a = 120.0F;
+        dst_reg[12] = a;
+        a = dst_reg[12];
+    }
+
+    {
+        // imm store, non-imm load
+        vFloat a = 120.0F;
+        dst_reg[12] = a;
+        a = dst_reg[imm - 23];
+    }
+
+    {
+        // non-imm store, imm load
+        vFloat a = 130.0F;
+        dst_reg[imm - 23] = a;
+        a = dst_reg[12];
+    }
+
+    {
+        // non-imm store, non-imm load
+        vFloat a = 140.0F;
+        dst_reg[imm - 23] = a;
+        a = dst_reg[imm - 23];
+    }
+}
+
+void basic_incrwc_nonop(int imm)
+{
+    {
+        // imm store, imm load
+        vFloat a = 120.0F;
+        dst_reg[12] = a;
+        dst_reg++;
+        a = dst_reg[12];
+    }
+
+    {
+        // imm store, non-imm load
+        vFloat a = 120.0F;
+        dst_reg[12] = a;
+        dst_reg++;
+        a = dst_reg[imm - 23];
+    }
+
+    {
+        // non-imm store, imm load
+        vFloat a = 130.0F;
+        dst_reg[imm - 23] = a;
+        dst_reg++;
+        a = dst_reg[12];
+    }
+
+    {
+        // non-imm store, non-imm load
+        vFloat a = 140.0F;
+        dst_reg[imm - 23] = a;
+        dst_reg++;
+        a = dst_reg[imm - 23];
+    }
+}
+
+void loop_nop()
+{
+    dst_reg[0] = 1.0f;
+    #pragma GCC unroll 0
+    for (int i = 0; i < 4; i++) {
+        vFloat a = dst_reg[12];
+        dst_reg[0] = a;
+    }
+}
+
+void loop_nonop()
+{
+    dst_reg[0] = 1.0f;
+    #pragma GCC unroll 0
+    for (int i = 0; i < 4; i++) {
+        vFloat a = 1.0f;
+        vFloat trash = dst_reg[0];
+        dst_reg[0] = a;
+    }
+}
+
+void loop_nop(int imm)
+{
+    dst_reg[0] = 1.0f;
+    #pragma GCC unroll 0
+    for (int i = 0; i < 4; i++) {
+        vFloat a = dst_reg[imm];
+        dst_reg[0] = a;
+    }
+}
