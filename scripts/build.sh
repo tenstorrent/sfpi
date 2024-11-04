4c5cb14cd05b5c5df56a9b120e16744b83d39cfa
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-11-04 16:07:40 +0000

    #14590: Get off lfs
    Remove intermediate sfpi-tt-gcc submodule
    Move gcc, binutils, newlib submodules directly here.
    Add build and release scripts.

---
+++ scripts/build.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+set -eo pipefail
+BIN="$(dirname "$0")"
+
+NCPUS=$(grep -c '^processor' /proc/cpuinfo)
+if test "$BIN" != "scripts"; then
+    echo "run this script from repo's top directory" 1>&2
+    exit 1
+fi
+
+if ! test -d build ; then
+    mkdir -p build/sfpi
+    # extract git hashes for here and each submodule
+    "$BIN/git-hash.sh" > build/sfpi/src-hashes
+fi
+
+# Clean the environment
+for env in $(printenv) ; do
+    var="${env%%=*}"
+    case "$var" in
+	LC_*) unset $var ;;
+    esac
+done
+export LC_ALL=C
+
+cd build
+test -e Makefile ||
+    ../configure --prefix="$(pwd)/sfpi/compiler" --disable-multilib --with-arch=rv32i --disable-gdb
+nice make -j${NCPUS:-1}
+cd ~-
+
