a0eb70aaff7455f344203463e9bb2a44198e2276
Nathan Sidwell <nsidwell@tenstorrent.com>
2024-12-19 10:32:22 -0500

    Add multilibs (#28)
    Add gs/wh/bh multilibs, augment build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,13 +17,15 @@ infra=false
 test=false
 test_gcc=false
 test_binutils=false
+gcc_checking=release
 while [ "$#" -ne 0 ] ; do
     case "$1" in
 	--serial) NCPUS=1 ;;
 	--infra) infra=true ;;
-	--test) infra=true ; test=true ;;
-	--test-gcc) infra=true ; test=false ; test_gcc=true ;;
-	--test-binutils) infra=true ; test=false ; test_binutils=true ;;
+	--test) infra=true test=true test_gcc=false test_binutils=false ;;
+	--test-gcc) infra=true test=false test_gcc=true ;;
+	--test-binutils) infra=true test=false test_binutils=true ;;
+	--checking=*) gcc_checking="${1#*=}" ;;
 	-*) echo "Unknown option '$1'" >2 ; exit 2 ;;
 	*) break ;;
     esac
@@ -51,18 +53,37 @@ done
 export LC_ALL=C
 
 # configure, if this is the first time
-test -e build/Makefile ||
-    (cd build ; ../configure --prefix="$(pwd)/sfpi/compiler" --disable-multilib --with-arch=rv32i --with-abi=ilp32 --disable-gdb)
+if ! test -e build/Makefile ; then
+    bugurl_option=
+    if test $(hostname | cut -d- -f-3) = 'tt-metal-dev' ; then
+	# Building at tenstorrent, I guess we're on the hook for it :)
+	bugurl_option=--with-bugurl='https://github.com/tenstorrent/tt-metal'
+    fi
+    (cd build
+     set -x
+     ../configure --prefix="$(pwd)/sfpi/compiler" $bugurl_option \
+		  --enable-gcc-checking="$gcc_checking" \
+		  --with-multilib-generator='rv32i_xttgs-ilp32-- rv32im_xttwh-ilp32-- rv32im_xttbh-ilp32--' \
+		  --with-arch=rv32i --with-abi=ilp32 --disable-gdb)
+fi
 
 # build the toolchain
-nice make -C build -j$NCPUS
+(set -x; nice make -C build -j$NCPUS)
 
 # maybe the test infra
-$infra && nice make -C build infra -j$NCPUS
+if $infra ; then
+    (set -x; nice make -C build infra -j$NCPUS)
+fi
 
 # maybe test
-$test && nice make -C build check -j$NCPUS
+if $test ; then
+    (set -x; nice make -C build check -j$NCPUS)
+fi
 
-$test_binutils && nice make -C build check-binutils -j$NCPUS
+if $test_binutils ; then
+    (set -x; nice make -C build check-binutils -j$NCPUS)
+fi
 
-$test_gcc && nice make -C build check-gcc -j$NCPUS
+if $test_gcc ; then
+   (set -x; nice make -C build check-gcc -j$NCPUS)
+fi
