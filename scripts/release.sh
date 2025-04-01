5fdc4d4ed7b60e2dab1659246f93ebbcbe07bd0d
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-04-01 18:33:29 +0000

    Add --force to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,9 +9,11 @@ if ! test "$BIN" -ef "scripts"; then
 fi
 
 BUILD=build
+force=false
 while [ "$#" -ne 0 ] ; do
     case "$1" in
 	--dir=*) BUILD="${1#*=}" ;;
+	--force) force=true ;;
 	-*) echo "Unknown option '$1'" >&2 ; exit 2 ;;
 	*) break ;;
     esac
@@ -22,7 +24,9 @@ done
 "$BIN/git-hash.sh" > $BUILD/src-hashes.post
 if ! cmp -s $BUILD/sfpi/src-hashes $BUILD/src-hashes.post ; then
     echo "source tree has changed since build started" 1>&2
-    exit 1
+    if ! $force ; then
+	exit 1
+    fi
 fi
 
 # Copy include tree
