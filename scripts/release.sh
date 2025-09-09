49e16c76fcbe2e8110b6a85b1580874a2efcb233
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-09-09 13:26:29 +0000

    Change release package naming convention

--- scripts/release.sh
+++ scripts/release.sh
@@ -52,7 +52,7 @@ tar cf - include | tar xf - -C $BUILD/sfpi
 find $BUILD/sfpi/compiler -type f -executable -exec file {} \; | \
     grep '^[^ ]*:  *ELF 64-bit ' | cut -d: -f1 | xargs strip -g
 
-NAME=sfpi-$(uname -m)_$(uname -s)
+NAME=sfpi_${tt_version:-UNKNOWN}_$(uname -m)
 
 # whether we can build a particular package type
 mkdeb=true
