36823de85ccc1314f17f587c06e0a6508e766fb9
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-10-09 16:23:19 +0000

    30304 Adjust release file naming

--- scripts/release.sh
+++ scripts/release.sh
@@ -15,12 +15,14 @@ tt_built=false
 if test $(hostname | cut -d- -f-3) = 'tt-metal-dev' ; then
     tt_built=true
 fi
+txz_only=false
 while [ "$#" -ne 0 ] ; do
     case "$1" in
 	--ci) ci=true ;;
 	--dir=*) BUILD="${1#*=}" ;;
 	--force) force=true ;;
 	--tt-built) tt_built=true ;;
+	--txz-only) txz_only=true ;;
 	-*) echo "Unknown option '$1'" >&2 ; exit 2 ;;
 	*) break ;;
     esac
@@ -52,106 +54,107 @@ tar cf - include | tar xf - -C $BUILD/sfpi
 find $BUILD/sfpi/compiler -type f -executable -exec file {} \; | \
     grep '^[^ ]*:  *ELF 64-bit ' | cut -d: -f1 | xargs strip -g
 
-NAME=sfpi_${tt_version:-UNKNOWN}_$(uname -m)
+eval $($BIN/sfpi-version.sh RELEASE $tt_version)
 
-# whether we can build a particular package type
-mkdeb=true
-mkrpm=false
-type rpmbuild >/dev/null 2>&1 && mkrpm=true
+rm -rf $BUILD/release
+mkdir $BUILD/release
 
-# whether we're on a particular package type
-ondeb=false
-onrpm=false
-dpkg-query -f '${Version}' -W libc6 >/dev/null 2>&1 && ondeb=true
-! $ondeb && rpm -q --qf '%{VERSION}' glibc >/dev/null 2>&1 && onrpm=true
+tar cJf $BUILD/release/$sfpi_filename.txz -C $BUILD sfpi
+echo "INFO: Tarball: $BUILD/release/$sfpi_filename.txz"
 
-fpm_options=(-s dir -n sfpi -C $BUILD/sfpi --prefix /opt/tenstorrent/sfpi)
-fpm_options+=(--architecture native)
-fpm_options+=(--license "Apache 2.0/GPL 2,3/LGPL 2.1")
-fpm_options+=(--url "https://github.com/tenstorrent/sfpi")
-fpm_options+=(--description "Tenstorrent SFPI compiler tools")
-if $tt_built ; then
-    fpm_options+=(--maintainer "Tenstorrent <support@tenstorrent.com>" --vendor "Tenstorrent")
-else
-    fpm_options+=(--maintainer Unmaintained --vendor Unknown)
-fi
+if ! $txz_only ; then
+    # Get the set of shared objects we use and figure what packages they come from.
+    # tuples of debian:fedora names
+    pkgs=
+    for so in $(find $BUILD/sfpi/compiler -type f -executable -exec file {} \; | \
+		    grep '^[^ ]*:  *ELF 64-bit ' | cut -d: -f1 | xargs -n1 ldd | \
+		    sed -e '/:$/d' -e "s/^[ \t]\+//" -e 's/ .*//' | sort -u)
+    do
+	case $so in
+	    /*/ld-linux-*.so*) ;; # loader
+	    linux-vdso.so*) ;;  # kernel vdso
+	    libc.so*) pkgs+='libc6:glibc' ;; # c library
+	    libexpat.so*) pkgs+=" libexpat1:expat";;
+	    libgcc_s.so*) ;; # compiler support
+	    libgmp.so*) pkgs+=" libgmp10:gmp";;
+	    libisl.so*) pkgs+=" libisl23:isl";;
+	    liblzma.so*) pkgs+=" liblzma5:xz-libs";;
+	    libm.so*) ;; # c library
+	    libmpc.so*) pkgs+=" libmpc3:libmpc";;
+	    libmpfr.so*) pkgs+=" libmpfr6:mpfr";;
+	    libncursesw.so*) pkgs+=" libncursesw6:ncurses-libs";;
+	    libstdc++.so*) pkgs+=" libstdc++6:libstdc++" ;; # C++ std lib 
+	    libtinfo.so*) pkgs+=" libtinfo6:ncurses-libs";;
+	    libz.so*) pkgs+=" zlib1g:zlib-ng-compat";;
+	    libzstd.so*) pkgs+=" libzstd1:libzstd";;
+	    *) echo "WARNING: unknown shared object $so" >&2 ;;
+	esac
+    done
 
-# Get the set of shared objects we use and figure what packages they come from.
-pkgs=
-for so in $(find $BUILD/sfpi/compiler -type f -executable -exec file {} \; | \
-		grep '^[^ ]*:  *ELF 64-bit ' | cut -d: -f1 | xargs -n1 ldd | \
-		sed -e '/:$/d' -e "s/^[ \t]\+//" -e 's/ .*//' | sort -u)
-do
-    case $so in
-	/*/ld-linux-*.so*) ;; # loader
-	linux-vdso.so*) ;;  # kernel vdso
-	libc.so*) pkgs+='libc6:glibc' ;; # c library
-	libexpat.so*) pkgs+=" libexpat1:expat";;
-	libgcc_s.so*) ;; # compiler support
-	libgmp.so*) pkgs+=" libgmp10:gmp";;
-	libisl.so*) pkgs+=" libisl23:isl";;
-	liblzma.so*) pkgs+=" liblzma5:xz-libs";;
-	libm.so*) ;; # c library
-	libmpc.so*) pkgs+=" libmpc3:libmpc";;
-	libmpfr.so*) pkgs+=" libmpfr6:mpfr";;
-	libncursesw.so*) pkgs+=" libncursesw6:ncurses-libs";;
-	libstdc++.so*) pkgs+=" libstdc++6:libstdc++" ;; # C++ std lib 
-	libtinfo.so*) pkgs+=" libtinfo6:ncurses-libs";;
-	libz.so*) pkgs+=" zlib1g:zlib-ng-compat";;
-	*) echo "WARNING: unknown shared object $so" >&2 ;;
-    esac
-done
-
-deb_deps=()
-rpm_dep=()
-
-for pkg in $pkgs
-do
-    if $ondeb ; then
-	if version=$(dpkg-query -f '${Version}' -W ${pkg/:*/} 2>/dev/null) ; then
-	    if [[ $version =~ ^([0-9]+:)?([0-9.]*) ]] ; then
-		version=${BASH_REMATCH[2]}
+    case $sfpi_dist in
+	linux)
+	    echo "INFO: Building packages for common linux distro kinds"
+	    fpm_options=(-s dir -n sfpi -C $BUILD/sfpi --prefix /opt/tenstorrent/sfpi)
+	    fpm_options+=(--architecture native)
+	    fpm_options+=(--license "Apache 2.0/GPL 2,3/LGPL 2.1")
+	    fpm_options+=(--url "https://github.com/tenstorrent/sfpi")
+	    fpm_options+=(--description "Tenstorrent SFPI compiler tools")
+	    if $tt_built ; then
+		fpm_options+=(--maintainer "Tenstorrent <support@tenstorrent.com>" \
+					   --vendor "Tenstorrent")
 	    else
-		version=
+		fpm_options+=(--maintainer Unmaintained --vendor Unknown)
 	    fi
-	fi
-    elif $onrpm ; then
-	if version=$(rpm -q --qf '%{VERSION}' ${pkg/*:/} 2>/dev/null) ; then
-	    :
-	fi
-    else
-	version=
-    fi
-    if test -z "$version" ; then
-	echo "WARNING: Cannot determine $pkg version used" >&2
-    else
-	echo "INFO: $pkg >= $version"
-	deb_deps+=(--depends "${pkg/:*/} >= $version")
-	rpm_deps+=(--depends "${pkg/*:/} >= $version")
-    fi
-done
 
-rm -f $BUILD/$NAME.txz
-tar cJf $BUILD/$NAME.txz -C $BUILD sfpi
-echo "INFO: Tarball: $BUILD/$NAME.txz"
-md5=txz
+	    deb_deps=()
+	    rpm_dep=()
 
-deb=true
-if $mkdeb ; then
-    rm -f $BUILD/$NAME.deb
-    # _ in version is verboten
-    fpm -t deb -v "${tt_version//_/-}" "${fpm_options[@]}" "${deb_deps[@]}" -p $BUILD/$NAME.deb
-    echo "INFO: Debian: $BUILD/$NAME.deb"
-    md5+=,deb
+	    for pkg in $pkgs
+	    do
+		case $sfpi_releaser in
+		    debian)
+			if version=$(dpkg-query -f '${Version}' -W ${pkg/:*/} 2>/dev/null) ; then
+			    if [[ $version =~ ^([0-9]+:)?([0-9.]*) ]] ; then
+				version=${BASH_REMATCH[2]}
+			    else
+				version=
+			    fi
+			fi
+			;;
+		    fedora)
+			if version=$(rpm -q --qf '%{VERSION}' ${pkg/*:/} 2>/dev/null) ; then
+			    :
+			fi
+			;;
+		    *)
+			version=
+			;;
+		esac
+		if test -z "$version" ; then
+		    echo "WARNING: Cannot determine $pkg version used" >&2
+		else
+		    echo "INFO: $pkg >= $version"
+		    deb_deps+=(--depends "${pkg/:*/} >= $version")
+		    rpm_deps+=(--depends "${pkg/*:/} >= $version")
+		fi
+	    done
+
+	    # _ in version is verboten
+	    fpm -t deb -v "${tt_version//_/-}" "${fpm_options[@]}" "${deb_deps[@]}" \
+		-p $BUILD/release/$sfpi_filename.deb
+	    echo "INFO: Debian: $BUILD/release/$sfpi_filename.deb"
+
+	    # - in version is verboten
+	    fpm -t rpm -v "${tt_version//-/_}" "${fpm_options[@]}" "${rpm_deps[@]}" \
+		-p $BUILD/release/$sfpi_filename.rpm \
+		--rpm-auto-add-directories --rpm-rpmbuild-define "_build_id_links none"
+	    echo "INFO: RPM: $BUILD/release/$sfpi_filename.rpm"
+	    ;;
+	*)
+	    echo "WARNING: Unknown packaging system on $sfpi_releaser" >&2
+	    ;;
+    esac
 fi
 
-if $mkrpm ; then
-    rm -f $BUILD/$NAME.rpm
-    # - in version is verboten
-    fpm -t rpm -v "${tt_version//-/_}" "${fpm_options[@]}" "${rpm_deps[@]}" -p $BUILD/$NAME.rpm \
-	--rpm-auto-add-directories --rpm-rpmbuild-define "_build_id_links none"
-    echo "INFO: RPM: $BUILD/$NAME.rpm"
-    md5+=,rpm
-fi
-(cd $BUILD ; md5sum -b $(eval echo $NAME.{$md5})) > $BUILD/$NAME.md5
-echo "INFO: MD5: $BUILD/$NAME.md5"
+(cd $BUILD/release ; md5sum -b $sfpi_filename.* > $sfpi_filename.md5)
+echo "INFO: MD5: $BUILD/release/$sfpi_filename.md5"
---
+++ scripts/sfpi-version.sh
@@ -0,0 +1,113 @@
+#! /bin/bash
+
+# Compute SFPI release version information.
+# Emit as evaluable shell assignments or CMAKE script
+# This is the source of truth as to how we determine arch and distro names.
+# Canonical location is in tenstorrent/tt-sfpi project's script directory.
+
+# Define release, update this bit for new release
+sfpi_version=7.5.0
+sfpi_x86_64_linux_txz_md5=...
+
+# One ring to rule them all,
+# One ring to find them,
+# One ring to bring them all
+# and in the darkness bind them
+
+# For the realm of mortals, using the toolchain
+# eval local $($FILE SHELL [$pkg]) -- generate sfpi variables to install
+# $FILE CMAKE [$pkg] -- emit cmake script to set variables
+
+# For the realm of elves, releasing the toolchain
+# eval $($FILE RELEASE $version) -- generate sfpi variables for release
+# $FILE *.md5 -- emit variable initializations from md5 file(s)
+
+set -e
+
+if [[ ${#1} = 0 ]] ; then
+    cat >&2 <<EOF
+Usage:
+$0 *.md5	 - generate release information
+$0 RELEASE \$VER - generate release names
+$0 SHELL [\$PKG] - shell use for PKG
+$0 CMAKE [\$PKG] - CMAKE use for PKG
+EOF
+    exit 1
+fi
+
+if [[ ${1-} =~ '.md5'$ ]] ; then
+    # convert md5 files into release variables, to insert above
+    version=
+    for file in "$@"
+    do
+	ver="${file##*/sfpi_}"
+	ver="${ver%%_*}"
+	if [[ $ver != $version ]] ; then
+	    version=$ver
+	    echo sfpi_version=$version
+	fi
+    done
+   sed 's/^\([0-9a-f]*\) \*sfpi_[^_]*_\([^.]*\)\.\(.*\)$/sfpi_\2_\3_md5=\1/' "$@"
+   exit 0
+fi
+
+if [[ ${1-} = RELEASE ]] ; then
+    # releaser of sfpi-version
+    sfpi_version=$2
+fi
+
+# define host system
+sfpi_dist=unknown
+sfpi_pkg=
+if [[ -r /etc/os-release ]] ; then
+    source /etc/os-release
+    # See if ID_LIKE indicates a debian or fedora clone
+    for like in $ID_LIKE
+    do
+	case $like in
+	    debian) ID=debian; break;;
+	    fedora) ID=fedora; break;;
+	esac
+    done
+
+    if [[ ${1-} = RELEASE ]] ; then
+	sfpi_releaser=$ID
+    fi
+
+    # debian and fedora are sufficiently close to treat as one, modulo
+    # packaging system. We endevor to build on a common denominator
+    # system and translate package dependencies.
+    case $ID in
+	debian) sfpi_dist=linux sfpi_pkg=deb;;
+	fedora) sfpi_dist=linux sfpi_pkg=rpm;;
+	*) sfpi_dist=$ID;;
+    esac
+fi
+sfpi_arch=$(uname -m)
+
+# define download location & name
+sfpi_repo=https://github.com/tenstorrent/sfpi
+sfpi_url=$sfpi_repo/releases/download/$sfpi_version
+sfpi_filename=sfpi_${sfpi_version}_${sfpi_arch}_${sfpi_dist}
+
+if [[ ${1-} != RELEASE ]] ; then
+    # querier of sfpi-version
+    if [[ -n $2 ]] ; then
+	sfpi_pkg=$2
+    fi
+    sfpi_filename+=".$sfpi_pkg"
+    sfpi_md5=$(eval echo "\${sfpi_${sfpi_arch}_${sfpi_dist}_${sfpi_pkg}_md5-}")
+    unset sfpi_builton
+fi
+
+# now emit definitions
+for var in $(set -o posix ; set | sed -e '/^sfpi_/{s/=.*//;p}' -e d)
+do
+    eval val="\$$var"
+    # relies on no inserted quoting for meta-characters
+    if [[ ${1-} = CMAKE ]] ; then
+	echo "set(SFPI${var#sfpi} \"$val\")"
+    else
+	echo "${var}='$val'"
+    fi
+done
