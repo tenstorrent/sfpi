c3a05d578cfd45b640fbf05b65d8b124d9d75b6a
Nathan Sidwell <nsidwell@tenstorrent.com>
2025-12-23 23:12:25 +0000

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -54,7 +54,7 @@ fi
 
 # figure version, now we know tt_built
 if [[ -r $BUILD/version ]]; then
-    eval $BUILD/version
+    source $BUILD/version
 elif [[ "$tt_version" == "" ]]; then
     # match and exclude are globs, not regexes. This is close enough.
     if ! tt_version=$(git describe --tags --match '[0-9]*.[0-9]*.[0-9]*' \
