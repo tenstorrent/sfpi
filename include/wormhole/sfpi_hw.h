3f02ca014d72625f6abc8ae99a6b2d287a2c7f58
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-02-03 13:19:16 +0000

    #26462: Fix and split sfpshft2 variants

--- include/wormhole/sfpi_hw.h
+++ include/wormhole/sfpi_hw.h
@@ -78,12 +78,6 @@ using __rvtt_vec_t = ::__xtt_vector;
 #define __builtin_rvtt_sfpstochrnd_i(mode, imm8, srcc, mod1) __builtin_rvtt_wh_sfpstochrnd_i(ckernel::instrn_buffer, mode, imm8, 0, 0, srcc, mod1)
 #define __builtin_rvtt_sfpstochrnd_v(mode, srcb, srcc, mod1) __builtin_rvtt_wh_sfpstochrnd_v(mode, srcb, srcc, mod1)
 
-#define __builtin_rvtt_sfpshft2_i(dst, imm) __builtin_rvtt_wh_sfpshft2_i(dst, imm)
-#define __builtin_rvtt_sfpshft2_v(dst, src) __builtin_rvtt_wh_sfpshft2_v(dst, src)
-#define __builtin_rvtt_sfpshft2_g(l0, l1, l2, l3, mod) __builtin_rvtt_wh_sfpshft2_g(l0, l1, l2, l3, mod)
-#define __builtin_rvtt_sfpshft2_ge(src, l0, l1, l2, l3) __builtin_rvtt_wh_sfpshft2_ge(src, l0, l1, l2, l3)
-#define __builtin_rvtt_sfpshft2_e(src, mod) __builtin_rvtt_wh_sfpshft2_e(src, mod)
-
 #define __builtin_rvtt_sfpconfig_v(l0, config_dest) __builtin_rvtt_wh_sfpconfig_v(l0, config_dest)
 
 constexpr unsigned int SFP_LREG_COUNT = 8;
