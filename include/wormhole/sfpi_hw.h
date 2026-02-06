e4ba2cded3f74f84c6e03b794f87d8eb02f2d6e5
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-02-06 16:16:38 +0000

    #35446: Unify some builtins

--- include/wormhole/sfpi_hw.h
+++ include/wormhole/sfpi_hw.h
@@ -22,12 +22,10 @@ using __rvtt_vec_t = ::__xtt_vector;
 #define __builtin_rvtt_sfpxcondb(s, i) __builtin_rvtt_sfpxcondb(s, i)
 #define __builtin_rvtt_sfpxcondi(i) __builtin_rvtt_sfpxcondi(i)
 
-#define __builtin_rvtt_sfpassign_lv(v, in) __builtin_rvtt_wh_sfpassign_lv(v, in)
 #define __builtin_rvtt_sfpload(mod0, mode, addr) __builtin_rvtt_wh_sfpload(ckernel::instrn_buffer, mod0, mode, addr, 0, 0)
 
 #define __builtin_rvtt_sfpxloadi(mod0, imm16) __builtin_rvtt_wh_sfpxloadi(ckernel::instrn_buffer, mod0, imm16, 0, 0)
 #define __builtin_rvtt_sfpstore(src, mod0, mode, addr) __builtin_rvtt_wh_sfpstore(ckernel::instrn_buffer, src, mod0, mode, addr, 0, 0)
-#define __builtin_rvtt_sfpmov(src, mod1) __builtin_rvtt_wh_sfpmov(src, mod1)
 
 #define __builtin_rvtt_sfpencc(imm12, mod1) __builtin_rvtt_wh_sfpencc(imm12, mod1)
 #define __builtin_rvtt_sfppushc() __builtin_rvtt_wh_sfppushc(0)
@@ -42,24 +40,18 @@ using __rvtt_vec_t = ::__xtt_vector;
 #define __builtin_rvtt_sfpmul(va, vb, mod1) __builtin_rvtt_wh_sfpmul(va, vb, mod1)
 #define __builtin_rvtt_sfpmad(va, vb, vc, mod1) __builtin_rvtt_wh_sfpmad(va, vb, vc, mod1)
 
-#define __builtin_rvtt_sfpexexp(src, mod1) __builtin_rvtt_wh_sfpexexp(src, mod1)
-#define __builtin_rvtt_sfpexman(src, mod1) __builtin_rvtt_wh_sfpexman(src, mod1)
-
 #define __builtin_rvtt_sfpsetexp_i(imm12, src) __builtin_rvtt_wh_sfpsetexp_i(ckernel::instrn_buffer, imm12, 0, 0, src)
 #define __builtin_rvtt_sfpsetexp_v(dst, src) __builtin_rvtt_wh_sfpsetexp_v(dst, src)
 
 #define __builtin_rvtt_sfpsetman_i(imm12, src, mod) __builtin_rvtt_wh_sfpsetman_i(ckernel::instrn_buffer, imm12, 0, 0, src, mod)
 #define __builtin_rvtt_sfpsetman_v(dst, src) __builtin_rvtt_wh_sfpsetman_v(dst, src)
 
-#define __builtin_rvtt_sfpabs(src, mod1) __builtin_rvtt_wh_sfpabs(src, mod1)
 #define __builtin_rvtt_sfpand(dst, src) __builtin_rvtt_wh_sfpand(dst, src)
 #define __builtin_rvtt_sfpor(dst, src) __builtin_rvtt_wh_sfpor(dst, src)
 #define __builtin_rvtt_sfpxor(dst, src) __builtin_rvtt_wh_sfpxor(dst, src)
 #define __builtin_rvtt_sfpnot(src) __builtin_rvtt_wh_sfpnot(src)
 
 #define __builtin_rvtt_sfpdivp2(imm12, src, mod1) __builtin_rvtt_wh_sfpdivp2(ckernel::instrn_buffer, imm12, 0, 0, src, mod1)
-
-#define __builtin_rvtt_sfplz(src, mod1) __builtin_rvtt_wh_sfplz(src, mod1)
 
 #define __builtin_rvtt_sfpshft_i(dst, imm12) __builtin_rvtt_wh_sfpshft_i(ckernel::instrn_buffer, dst, imm12, 0, 0)
 #define __builtin_rvtt_sfpshft_v(dst, src) __builtin_rvtt_wh_sfpshft_v(dst, src)
