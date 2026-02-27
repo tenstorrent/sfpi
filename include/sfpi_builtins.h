b42999c8ed6df45f3ea902ed2c7bc080d20970be
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-02-27 19:59:44 +0000

    Reorder setman,exp,sgn operands

--- include/sfpi_builtins.h
+++ include/sfpi_builtins.h
@@ -18,9 +18,9 @@ namespace sfpi {
 #define __builtin_rvtt_sfpshft_i(dst, imm12, mod1) __builtin_rvtt_sfpshft_i(ckernel::instrn_buffer, dst, imm12, 0, 0, mod1)
 #define __builtin_rvtt_sfpload(mod0, mode, addr) __builtin_rvtt_sfpload(ckernel::instrn_buffer, mod0, mode, addr, 0, 0)
 #define __builtin_rvtt_sfpstore(src, mod0, mode, addr) __builtin_rvtt_sfpstore(ckernel::instrn_buffer, src, mod0, mode, addr, 0, 0)
-#define __builtin_rvtt_sfpsetexp_i(imm12, src) __builtin_rvtt_sfpsetexp_i(ckernel::instrn_buffer, imm12, 0, 0, src)
-#define __builtin_rvtt_sfpsetman_i(imm12, src) __builtin_rvtt_sfpsetman_i(ckernel::instrn_buffer, imm12, 0, 0, src)
-#define __builtin_rvtt_sfpsetsgn_i(imm12, src) __builtin_rvtt_sfpsetsgn_i(ckernel::instrn_buffer, imm12, 0, 0, src)
+#define __builtin_rvtt_sfpsetexp_i(src, imm12) __builtin_rvtt_sfpsetexp_i(ckernel::instrn_buffer, src, imm12, 0, 0)
+#define __builtin_rvtt_sfpsetman_i(src, imm12) __builtin_rvtt_sfpsetman_i(ckernel::instrn_buffer, src, imm12, 0, 0)
+#define __builtin_rvtt_sfpsetsgn_i(src, imm12) __builtin_rvtt_sfpsetsgn_i(ckernel::instrn_buffer, src, imm12, 0, 0)
 #define __builtin_rvtt_sfpdivp2(imm12, src, mod1) __builtin_rvtt_sfpdivp2(ckernel::instrn_buffer, imm12, 0, 0, src, mod1)
 #define __builtin_rvtt_sfpstochrnd_i(mode, imm8, srcc, mod1) __builtin_rvtt_sfpstochrnd_i(ckernel::instrn_buffer, mode, imm8, 0, 0, srcc, mod1)
 
