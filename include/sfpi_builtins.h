eabe3d79dc1024b7edec534e2f031879fa61f159
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-03-05 14:42:19 +0000

    #35446: Reorder sfpxloadi operands

--- include/sfpi_builtins.h
+++ include/sfpi_builtins.h
@@ -12,17 +12,17 @@
 namespace sfpi {
 
 // Argument reordering here is temporary shim
-#define __builtin_rvtt_sfpxicmps(v, i, mod1) __builtin_rvtt_sfpxicmps(ckernel::instrn_buffer, v, i, 0, 0, mod1)
-#define __builtin_rvtt_sfpxiadd_i(src, imm12, mod1) __builtin_rvtt_sfpxiadd_i(ckernel::instrn_buffer, src, imm12, 0, 0, mod1)
-#define __builtin_rvtt_sfpxloadi(imm16, mod0) __builtin_rvtt_sfpxloadi(ckernel::instrn_buffer, mod0, imm16, 0, 0)
-#define __builtin_rvtt_sfpxfcmps(v, f, mod1) __builtin_rvtt_sfpxfcmps(ckernel::instrn_buffer, v, f, 0, 0, mod1)
-#define __builtin_rvtt_sfpshft_i(dst, imm12, mod1) __builtin_rvtt_sfpshft_i(ckernel::instrn_buffer, dst, imm12, 0, 0, mod1)
+#define __builtin_rvtt_sfpxicmps(src, imm, mod1) __builtin_rvtt_sfpxicmps(ckernel::instrn_buffer, src, imm, 0, 0, mod1)
+#define __builtin_rvtt_sfpxfcmps(src, imm, mod1) __builtin_rvtt_sfpxfcmps(ckernel::instrn_buffer, src, imm, 0, 0, mod1)
+#define __builtin_rvtt_sfpxiadd_i(src, imm, mod1) __builtin_rvtt_sfpxiadd_i(ckernel::instrn_buffer, src, imm, 0, 0, mod1)
+#define __builtin_rvtt_sfpxloadi(imm, mod0) __builtin_rvtt_sfpxloadi(ckernel::instrn_buffer, imm, 0, 0, mod0)
+#define __builtin_rvtt_sfpshft_i(src, imm, mod1) __builtin_rvtt_sfpshft_i(ckernel::instrn_buffer, src, imm, 0, 0, mod1)
 #define __builtin_rvtt_sfpload(addr, mod0, mode) __builtin_rvtt_sfpload(ckernel::instrn_buffer, addr, 0, 0, mod0, mode)
 #define __builtin_rvtt_sfpstore(src, addr, mod0, mode) __builtin_rvtt_sfpstore(ckernel::instrn_buffer, src, addr, 0, 0, mod0, mode)
-#define __builtin_rvtt_sfpsetexp_i(src, imm12) __builtin_rvtt_sfpsetexp_i(ckernel::instrn_buffer, src, imm12, 0, 0)
-#define __builtin_rvtt_sfpsetman_i(src, imm12) __builtin_rvtt_sfpsetman_i(ckernel::instrn_buffer, src, imm12, 0, 0)
-#define __builtin_rvtt_sfpsetsgn_i(src, imm12) __builtin_rvtt_sfpsetsgn_i(ckernel::instrn_buffer, src, imm12, 0, 0)
-#define __builtin_rvtt_sfpdivp2(src, imm12, mod1) __builtin_rvtt_sfpdivp2(ckernel::instrn_buffer, src, imm12, 0, 0, mod1)
-#define __builtin_rvtt_sfpstochrnd_i(srcc, imm8, mode, mod1) __builtin_rvtt_sfpstochrnd_i(ckernel::instrn_buffer, srcc, imm8, 0, 0, mode, mod1)
+#define __builtin_rvtt_sfpsetexp_i(src, imm) __builtin_rvtt_sfpsetexp_i(ckernel::instrn_buffer, src, imm, 0, 0)
+#define __builtin_rvtt_sfpsetman_i(src, imm) __builtin_rvtt_sfpsetman_i(ckernel::instrn_buffer, src, imm, 0, 0)
+#define __builtin_rvtt_sfpsetsgn_i(src, imm) __builtin_rvtt_sfpsetsgn_i(ckernel::instrn_buffer, src, imm, 0, 0)
+#define __builtin_rvtt_sfpdivp2(src, imm, mod1) __builtin_rvtt_sfpdivp2(ckernel::instrn_buffer, src, imm, 0, 0, mod1)
+#define __builtin_rvtt_sfpstochrnd_i(src, imm, mode, mod1) __builtin_rvtt_sfpstochrnd_i(ckernel::instrn_buffer, src, imm, 0, 0, mode, mod1)
 
 } // namespace sfpi
