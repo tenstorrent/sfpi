cb9c6612af44fd749b6afda4bb150093a9999f40
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-03-12 14:42:20 +0000

    #36930: Add sfploadi, replace sfpxloadi with sfpxloads

--- include/sfpi_funcs.h
+++ include/sfpi_funcs.h
@@ -187,9 +187,9 @@ sfpi::vFloat::vFloat (impl_::vLReg lr) : vVal (__builtin_rvtt_sfpreadlreg (lr.ge
 sfpi::vFloat::vFloat (impl_::vDReg dreg)
     : vVal (__builtin_rvtt_sfpload (dreg.get (), SFPLOAD_MOD0_FMT_SRCB, SFPLOAD_ADDR_MODE_NOINC)) {}
 sfpi::vFloat::vFloat (s2vFloat16 val)
-    : vVal (__builtin_rvtt_sfpxloadi (val.get (), val.get_format ())) {}
+    : vVal (__builtin_rvtt_sfploadi (val.get (), val.get_format ())) {}
 sfpi::vFloat::vFloat (float f)
-    : vVal (__builtin_rvtt_sfpxloadi (impl_::float_as_uint (f), SFPXLOADI_MOD0_FLOAT)) {}
+    : vVal (__builtin_rvtt_sfpxloads (impl_::float_as_uint (f), 0, 32)) {}
 
 auto sfpi::vFloat::operator= (vFloat in)-> vFloat & { assign (in.get ()); return *this; }
 auto sfpi::vFloat::operator= (impl_::vLReg lr)-> vFloat &  { impl_::vVal::operator= (lr); return *this; }
@@ -243,13 +243,13 @@ sfpi::vInt::vInt (impl_::vDReg dreg)
 }
 sfpi::vInt::vInt (vUInt val) : vVal (val.get ()) {}
 sfpi::vInt::vInt (int16_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPLOADI_MOD0_SHORT)) {}
+    : vVal (__builtin_rvtt_sfploadi (val, SFPLOADI_MOD0_SHORT)) {}
 sfpi::vInt::vInt (uint16_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPLOADI_MOD0_USHORT)) {}
+    : vVal (__builtin_rvtt_sfploadi (val, SFPLOADI_MOD0_USHORT)) {}
 sfpi::vInt::vInt (int32_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPXLOADI_MOD0_INT32)) {}
+    : vVal (__builtin_rvtt_sfpxloads (val, 1, 32)) {}
 sfpi::vInt::vInt (uint32_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPXLOADI_MOD0_UINT32)) {}
+    : vVal (__builtin_rvtt_sfpxloads (val, 0, 32)) {}
 sfpi::vInt::vInt (int val) : vInt (int32_t (val)) {}
 sfpi::vInt::vInt (unsigned val) : vInt (uint32_t (val)) {}
 sfpi::vInt::vInt(const impl_::vCond vc)
@@ -312,13 +312,13 @@ sfpi::vUInt::vUInt (impl_::vDReg dreg)
                                     SFPLOAD_ADDR_MODE_NOINC)) {}
 sfpi::vUInt::vUInt (vInt val) : vVal (val.get ()) {}
 sfpi::vUInt::vUInt (int16_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPLOADI_MOD0_SHORT)) {}
+    : vVal (__builtin_rvtt_sfploadi (val, SFPLOADI_MOD0_SHORT)) {}
 sfpi::vUInt::vUInt (uint16_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPLOADI_MOD0_USHORT)) {}
+    : vVal (__builtin_rvtt_sfploadi (val, SFPLOADI_MOD0_USHORT)) {}
 sfpi::vUInt::vUInt (int32_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPXLOADI_MOD0_INT32)) {}
+    : vVal (__builtin_rvtt_sfpxloads (val, 1, 32)) {}
 sfpi::vUInt::vUInt (uint32_t val)
-    : vVal (__builtin_rvtt_sfpxloadi (val, SFPXLOADI_MOD0_UINT32)) {}
+    : vVal (__builtin_rvtt_sfpxloads (val, 0, 32)) {}
 
 sfpi::vUInt::vUInt (int val) : vUInt (int32_t (val)) {}
 sfpi::vUInt::vUInt (unsigned val) : vUInt (uint32_t (val)) {}
