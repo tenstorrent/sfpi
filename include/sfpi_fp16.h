997156c86fd81de2ad3c1cf29503b072475fceda
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-04-03 18:52:42 +0000

    add stub files

---
+++ include/sfpi_fp16.h
@@ -0,0 +1,5 @@
+// File deleted 2026-04-03
+// Keep a stub version so that we don't leave the old one when installing ontop
+// of an existing install.
+// Shout at the user to fix their code
+#error "File deleted, you should never have included this in the first place"
