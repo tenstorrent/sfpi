6ca8e6bec9b83a2c0811c0ba85c3cf365681509c
Nathan Sidwell <nsidwell@tenstorrent.com>
2026-09-18 19:24:12 +0000

    Remove sfpi::round

--- include/sfpi_lib.h
+++ include/sfpi_lib.h
@@ -860,31 +860,6 @@ sfpi_inline void swap (Type &a, Type &b) {
   b = Type (__builtin_rvtt_sfpselect2 (r, 1));
 }
 
-namespace impl_ {
-class FloatInt : public std::pair<vFloat, vInt> {
-public:
-  using pair::pair;
-
-public:
-  operator vFloat () const { return first; }
-  operator vInt () const { return second; }
-};
-}
-
-enum class RoundIntMode {
-  Unbiased,
-  Biased,
-};
-
-sfpi_inline impl_::FloatInt round (vFloat x, RoundIntMode mode = RoundIntMode::Unbiased) {
-  vFloat magic = 0x1.8p23f;
-  auto f = x + magic;
-  auto i = as<vInt> (f);
-  if (mode == RoundIntMode::Unbiased)
-    i -= as<vInt> (magic);
-  return {f - magic, i};
-}
-
 __SFPI_DEPRECATED("Use sfpi::swap")
 sfpi_inline void vec_swap (vFloat & a, vFloat &b) = delete;
 __SFPI_DEPRECATED("Use sfpi::swap")
